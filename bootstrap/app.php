88e61186222d66488066f4fef882e016e1dcf980
itd3vbox <it.d3vbox@gmail.com>
2024-08-24 22:56:22 +0200

    sign-in & sign-out ready

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,7 +12,11 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->validateCsrfTokens(except: [
+            //'/*',
+            'http://projectmanager.demo/api/sign-in',
+        ]);
+        $middleware->statefulApi();
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
